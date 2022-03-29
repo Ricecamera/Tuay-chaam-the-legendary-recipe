0d0a7ae743011b3608d78af55fd9013ac18fe0bf
NatthasaKendo <kendokeyword@gmail.com>
2022-03-30 01:04:16 +0700

    Star in victory scene

---
+++ Assets/Scripts/LevelSelectionSystem/Scenes/VictoryScene.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryScene : MonoBehaviour
+{
+    private GameObject starL;
+    private GameObject starR;
+    private GameObject starM;
+
+    private void Start()
+    {
+        stars = GameObject.FindGameObjectsWithTag("Star");
+        foreach (GameObject star in stars)
+        {
+            if (star.contains("L"))
+            {
+                starL = star;
+            }
+            else if (starL.contains("R"))
+            {
+                starR = star;
+            }
+            else if (starR.contains("M"))
+            {
+                starM = star;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        // TODO set stars to correct state 
+        //* die = 0 -> 3 stars -> all star.GetComponent<Animator>().setTrigger("Start")
+        //* die = 1 -> 2 stars -> starR is not trigger
+        //* die = 2 -> 1 star -> starR and starM are not trigger
+        //* die = 3 -> 0 stars -> no star is trigger
+    }
+}
