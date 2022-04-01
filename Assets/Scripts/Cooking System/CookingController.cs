ed52b8aaee794c3f78be821adcce33ca3f88ad43
Peemajoke <zinger.peem@gmail.com>
2022-04-01 20:50:28 +0700

    Implement some part of cooking system

--- Assets/Scripts/Cooking System/CookingController.cs
+++ Assets/Scripts/Cooking System/CookingController.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CookingController : MonoBehaviour
 {
     //Food recipies DB.
-    private Dictionary<List<string>, Skill> recepies_1 = new Dictionary<List<string>, Skill>();
-    private Dictionary<List<string>, Skill> recepies_2 = new Dictionary<List<string>, Skill>();
-    private Dictionary<List<string>, Skill> recepies_3 = new Dictionary<List<string>, Skill>();
-    private Dictionary<List<string>, Skill> recepies_4 = new Dictionary<List<string>, Skill>();
-    private Dictionary<List<string>, Skill> recepies_5 = new Dictionary<List<string>, Skill>();
+    private Dictionary<List<string>, Skill> recepies = new Dictionary<List<string>, Skill>();
+
     // Start is called before the first frame update
     void Start()
     {
         //init all special skill and map it in the dict
+        recepies.Add(new List<string>(){"(Ingredients) honey", "(Ingredients) Lime", "(Ingredients) salt"}, new SetOneEnemyHPTo10("VGSP1", "GainSPOneAlliance", "Target one alliance and gain it 1 SP.", 3, Resources.Load("SkillIcons/sk3", typeof(Sprite)) as Sprite));
 
     }
 
-    void OnIngredientClick(){
+    void OnIngredientClick(List<string> selectedIngredient){
+        if(recepies.ContainsKey(selectedIngredient)){
+            GameObject describeText = GameObject.Find("describeText");
+            Text description = describeText.GetComponent<Text>();
+            description.text="Honey Lemon: It will deal great damage to one enemy.";
+            // set name of food
+            // recepies[selectedIngredient].SkillNamed;
 
+            // set description of food
+            // recepies[selectedIngredient].Description;
+
+            //enable the cook window
+        }else{
+            //disable the cook window
+
+        }
     }
 
-    void OnStartCooking(){
-
+    void OnStartCooking(List<string> selectedIngredient,PakRender caller, List<PakRender> target){
+        recepies[selectedIngredient].performSkill(caller, target);
     }
 }
