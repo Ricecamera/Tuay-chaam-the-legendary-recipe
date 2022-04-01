0e7b8aa246de4786d776932f19d692e4594abad3
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2022-04-02 01:26:44 +0700

    Add Base class for buff system

---
+++ Assets/Scripts/Buff system/BaseBuff.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BuffSystem {
+    
+    public class BaseBuff : ScriptableObject
+    {
+        public string buffName;     // unique name of this buff, can use as a key
+
+        public string description;  // description of the effect of this buff
+
+        public int duration;        // the number of turn this effect last
+
+        public Sprite buffIcon;
+    }
+}
