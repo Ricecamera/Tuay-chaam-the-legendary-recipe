2e33a01e3fa37b28d6aecda1c3fcaafed31b436e
Peemajoke <zinger.peem@gmail.com>
2022-04-02 20:33:57 +0700

    Temp b/f merge

--- Assets/Scripts/Cooking System/CookingController.cs
+++ Assets/Scripts/Cooking System/CookingController.cs
@@ -12,11 +12,12 @@ public class CookingController : MonoBehaviour
     void Start()
     {
         //init all special skill and map it in the dict
-        recepies.Add(new List<string>(){"(Ingredients) honey", "(Ingredients) Lime", "(Ingredients) salt"}, new SetOneEnemyHPTo10("VGSP1", "GainSPOneAlliance", "Target one alliance and gain it 1 SP.", 3, Resources.Load("SkillIcons/sk3", typeof(Sprite)) as Sprite));
+        recepies.Add(new List<string>(){"honey", "lime", "salt"}, new SetOneEnemyHPTo10("HTo10", "SetOneEnemyHPTo10", "Deal very great amount of damage to one enemy.", 1, Resources.Load("SkillIcons/sk3", typeof(Sprite)) as Sprite));
+        recepies.Add(new List<string>(){"carrot", "garlic", "honey", "lime", "salt"}, new HalfEnemiesHealth("HAEH", "HalfEnemiesHealth", "Half all enemies HP in half.", 3, Resources.Load("SkillIcons/sk3", typeof(Sprite)) as Sprite));
 
     }
 
-    void OnIngredientClick(List<string> selectedIngredient){
+    public void OnIngredientClick(List<string> selectedIngredient){
         if(recepies.ContainsKey(selectedIngredient)){
             // set description of food
             GameObject describeText = GameObject.Find("describeText");
@@ -30,10 +31,11 @@ public class CookingController : MonoBehaviour
             //disable the cook window
             GameObject describeText = GameObject.Find("describeText");
             describeText.SetActive(false);
+        
         }
     }
 
-    void OnStartCooking(List<string> selectedIngredient,PakRender caller, List<PakRender> target){
+    public void OnStartCooking(List<string> selectedIngredient,PakRender caller, List<PakRender> target){
         recepies[selectedIngredient].performSkill(caller, target);
     }
 }
