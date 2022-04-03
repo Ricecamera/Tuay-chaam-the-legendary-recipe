9900bafbfb6e69aa19652b010d3a390e728214e7
NatthasaKendo <kendokeyword@gmail.com>
2022-04-03 14:27:59 +0700

    improve main menu

--- Assets/Scripts/LevelSelectionSystem/Scenes/MainMenu.cs
+++ Assets/Scripts/LevelSelectionSystem/Scenes/MainMenu.cs
@@ -8,16 +8,15 @@ public class MainMenu : MonoBehaviour
     //! These buttons will be used in the future
     private Button newGameButton;
     private Button loadGameButton;
-    private Button playButton;
-    private Button loadButton;
     private Button quitButton;
+    private Button settingsButton;
 
     private void Start()
     {
         //! These buttons will be used in the future
         newGameButton = GameObject.Find("New Game").GetComponent<Button>();
         loadGameButton = GameObject.Find("Load Game").GetComponent<Button>();
-        playButton = GameObject.Find("Play").GetComponent<Button>();
+        settingsButton = GameObject.Find("Setting").GetComponent<Button>();
         quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
     }
 
@@ -28,7 +27,6 @@ public class MainMenu : MonoBehaviour
             //! These buttons will be used in the future
             newGameButton.onClick.AddListener(SceneLoader.Instance.LoadNextScene);
             loadGameButton.onClick.AddListener(SceneLoader.Instance.LoadNextScene);
-            playButton.onClick.AddListener(SceneLoader.Instance.LoadNextScene);
             quitButton.onClick.AddListener(() =>
             {
                 Debug.Log("Quit");
