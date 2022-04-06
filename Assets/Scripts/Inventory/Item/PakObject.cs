fb24652354e4f2d54d586e84a2adbc88b56c2f29
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2022-04-06 15:30:00 +0700

    PakObject will read data from pak Prefab

--- Assets/Scripts/Inventory/Item/PakObject.cs
+++ Assets/Scripts/Inventory/Item/PakObject.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Pak Object", menuName = "Inventory System/Items/PakItem")]
 public class PakObject : ItemObject
 {
+    List<string> skillName;
     private void Awake()
     {
         type = ItemType.Pak;
+        skillName = new List<string>();
         // Debug.Log(prefab.AddComponent<EggplantRender>().healthSystem.CurrentHp);
+    }
+
+    private void OnValidate() {
+        PakRender pak = prefab?.GetComponent<PakRender>();
+        if (pak != null) {
+            _name = pak.name;
+            uiDisplay = pak.Entity.image;
+            description = pak.Entity.Description;
+
+            for (int i = 0; i < pak.skills.Length; i++) {
+                skillName.Add(pak.skills[i].skillName);
+            }
+        }
     }
 }
